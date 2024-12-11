2c5abbb78bd860e0c3c465b2b6e22abcc4aba979
mengshuaiyang <mengshuaiyang@hotmail.com>
2024-12-11 15:18:35 +0800

    完善备忘录模式代码

---
+++ MementoPattern/3.Caretaker（监护人）/Caretaker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoPattern
+{
+    /// <summary>
+    /// 备忘录管理者
+    /// </summary>
+    public class Caretaker
+    {
+        private TextMemento _mementos;
+
+        public void SetMemento(TextMemento memento)
+        {
+            _mementos=memento;
+        }
+
+        public TextMemento GetMemento()
+        {
+            return _mementos;
+        }
+    }
+}
--- MementoPattern/Program.cs
+++ MementoPattern/Program.cs
@@ -10,17 +10,33 @@ namespace MementoPattern
     {
         static void Main(string[] args)
         {
-            TextEditor editor = new TextEditor();
-            editor.Type("Hello, World!");
-            Console.WriteLine(editor.GetText()); // 输出: Hello, World!
+            ////第一种实现方式
+            //TextEditor editor = new TextEditor();
+            //editor.Type("Hello, World!");
+            //Console.WriteLine(editor.GetText()); // 输出: Hello, World!
 
-            TextMemento save = editor.Save(); // 保存当前状态
+            //TextMemento save = editor.Save(); // 保存当前状态
 
-            editor.Type("Example");
-            Console.WriteLine(editor.GetText()); // 输出: Hello, World! Example
+            //editor.Type("Example");
+            //Console.WriteLine(editor.GetText()); // 输出: Hello, World! Example
 
-            editor.Restore(save); // 恢复到保存的状态
-            Console.WriteLine(editor.GetText()); // 输出: Hello, World!
+            //editor.Restore(save); // 恢复到保存的状态
+            //Console.WriteLine(editor.GetText()); // 输出: Hello, World!
+
+
+            //第二种实现方式
+            TextEditor editorOther = new TextEditor();
+            Caretaker caretaker= new Caretaker();
+
+            editorOther.Type("Hello, World!");
+            caretaker.SetMemento(editorOther.Save()); // 保存当前状态
+
+            editorOther.Type("Example");
+            Console.WriteLine(editorOther.GetText()); // 输出: Hello, World! Example
+
+            editorOther.Restore(caretaker.GetMemento()); // 恢复到保存的状态
+
+            Console.WriteLine(editorOther.GetText()); // 输出: Hello, World! Example
 
             Console.ReadKey();
         }
